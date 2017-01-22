e69558bd5c97d62fa7105d3b88da798ce2edb5f4
k9532121 <k9532121@gmail.com>
2017-01-22 15:00:24 +0800

    refactor Book.php

--- src/Book.php
+++ src/Book.php
@@ -5,6 +5,8 @@ class Book
 {
     private $data;
     private $oneBookPrice = 100;
+    private $allBookNumber = 5;
+
     function __construct(array $data)
     {
         $this->data = $data;
@@ -15,26 +17,28 @@ class Book
         $money = 0;
         do {
             $bookTmp = 0;
-            for ($i=0; $i < 5; $i++) {
+            for ($i=0; $i < $this->allBookNumber; $i++) {
                 if ($this->data[$i]>0) {
                     $bookTmp++;
                     $this->data[$i]--;
                 }
             }
-            if ($bookTmp==1) {
-                $money += $this->oneBookPrice;
-            }
-            if ($bookTmp==2) {
-                $money += ($bookTmp*$this->oneBookPrice*0.95);
-            }
-            if ($bookTmp==3) {
-                $money += ($bookTmp*$this->oneBookPrice*0.9);
-            }
-            if ($bookTmp==4) {
-                $money += ($bookTmp*$this->oneBookPrice*0.8);
-            }
-            if ($bookTmp==5) {
-                $money += ($bookTmp*$this->oneBookPrice*0.75);
+            switch ($bookTmp) {
+                case 1:
+                    $money += $this->oneBookPrice;
+                    break;
+                case 2:
+                    $money += ($bookTmp*$this->oneBookPrice*0.95);
+                    break;
+                case 3:
+                    $money += ($bookTmp*$this->oneBookPrice*0.9);
+                    break;
+                case 4:
+                    $money += ($bookTmp*$this->oneBookPrice*0.8);
+                    break;
+                case 5:
+                    $money += ($bookTmp*$this->oneBookPrice*0.75);
+                    break;
             }
         } while ($bookTmp!=0);
         return $money;
